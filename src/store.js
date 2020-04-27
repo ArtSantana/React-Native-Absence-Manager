42010cc62be789b76151028bbd19a3c40a28bef5
artsantana <artsantana1457@hotmail.com>
2020-04-26 23:04:51 -0300

    Removed useless file

--- src/store.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from 'redux';
-import AsyncStorage from '@react-native-community/async-storage';
-import { persistStore, persistReducer } from 'redux-persist';
-import reducer from './reducer';
