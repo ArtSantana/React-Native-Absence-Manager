6240aa032ca3938ade2cb39cbb0a5bc9cb254015
artsantana <artsantana1457@hotmail.com>
2020-04-26 21:50:42 -0300

    Created the firebaseConfig file and added to .gitignore

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Routes from './src/routes';
+import * as firebase from 'firebase';
+import firebaseConfig from './firebaseConfig';
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
