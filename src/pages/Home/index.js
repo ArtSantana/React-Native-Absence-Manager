6d00936c0d9f854a88ffcff58179c7157614eec2
artsantana <artsantana1457@hotmail.com>
2020-05-24 21:02:28 -0300

    Render subjects in FlatList, added logout button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,52 @@
-import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Text, FlatList, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {useDispatch, useSelector} from 'react-redux';
+import {Creators as AuthActions} from '../../store/ducks/auth';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntIcon from 'react-native-vector-icons/AntDesign';
 import Subject from '../../component/Subject';
 import styles from './styles';
 
 export default function Home() {
+  const subjects = useSelector((state) => state.subjects.subjects);
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(subjects);
+  }, []);
+
+  function logout() {
+    dispatch(AuthActions.logout());
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>notDP</Text>
+        <TouchableOpacity onPress={logout}>
+          <Icon name="logout" size={24} color="white" />
+        </TouchableOpacity>
       </View>
-      <Subject 
-        subject="Banco de dados"
-        teacher="Luciana Potker"
-        maxAbsences={20}
-        absences={4}
+      <FlatList
+        data={subjects}
+        keyExtractor={(subjects) => String(subjects.name)}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item: subjects}) => (
+          <Subject
+            subject={subjects.name}
+            teacher={subjects.teacher}
+            maxAbsences={subjects.maxAbsences}
+            absences={subjects.totalAbsences}
+          />
+        )}
       />
+      <TouchableOpacity
+        style={styles.plusIcon}
+        onPress={() => navigation.navigate('CreateSubject')}>
+        <AntIcon name="pluscircle" size={40} color="#AF52DE" />
+      </TouchableOpacity>
     </View>
   );
 }
-
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
   container: {
@@ -7,8 +7,10 @@ export default StyleSheet.create({
 
   header: {
     justifyContent: 'space-between',
+    flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: '#AF52DE',
+    paddingHorizontal: 8,
   },
 
   title: {
@@ -16,4 +18,11 @@ export default StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 28,
   },
-})
+
+  plusIcon: {
+    position: 'absolute',
+    bottom: 0,
+    alignSelf: 'center',
+    marginBottom: 16,
+  },
+});
